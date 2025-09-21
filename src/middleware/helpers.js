14266f5970596bf318525b48f48b22878e8d30f8
Adam Sultan <adamsult@andrew.cmu.edu>
2025-09-21 17:21:33 +0000

    Adam - From P1, refactored src/middleware/helpers.js

--- src/middleware/helpers.js
+++ src/middleware/helpers.js
@@ -27,34 +27,50 @@ helpers.try = function (middleware) {
 	};
 };
 
-helpers.buildBodyClass = function (req, res, templateData = {}) {
-	const clean = req.path.replace(/^\/api/, '').replace(/^\/|\/$/g, '');
+function normalizePath(path) {
+	return path.replace(/^\/api/, '').replace(/^\/|\/$/g, '');
+}
+
+function safeSlug(p) {
+	try {
+		p = slugify(decodeURIComponent(p));
+	} catch (err) {
+		winston.error(`Error decoding URI: ${p}`);
+		winston.error(err.stack);
+		p = '';
+	}
+	return validator.escape(String(p));
+}
+
+function buildSegments(clean) {
 	const parts = clean.split('/').slice(0, 3);
-	parts.forEach((p, index) => {
-		try {
-			p = slugify(decodeURIComponent(p));
-		} catch (err) {
-			winston.error(`Error decoding URI: ${p}`);
-			winston.error(err.stack);
-			p = '';
-		}
-		p = validator.escape(String(p));
-		parts[index] = index ? `${parts[0]}-${p}` : `page-${p || 'home'}`;
+	return parts.map((p, index, arr) => {
+		const safe = safeSlug(p);
+		return index ? `${safeSlug(arr[0])}-${safe}` : `page-${safe || 'home'}`;
 	});
-	const { template } = templateData;
+}
+
+function addTemplateName(parts, template) {
 	if (template) {
 		parts.push(`template-${template.name.split('/').join('-')}`);
 	}
+}
 
+function addTemplateTopic(parts, template, templateData) {
 	if (template && template.topic) {
 		parts.push(`page-topic-category-${templateData.category.cid}`);
 		parts.push(`page-topic-category-${slugify(templateData.category.name)}`);
 	}
+}
 
+function addTemplateChats(parts, template, templateData) {
 	if (template && template.chats && templateData.roomId) {
 		parts.push(`page-user-chats-${templateData.roomId}`);
 	}
+}
 
+
+function addTemplateBreadcrumbs(parts, templateData) {
 	if (Array.isArray(templateData.breadcrumbs)) {
 		templateData.breadcrumbs.forEach((crumb) => {
 			if (crumb && crumb.hasOwnProperty('cid')) {
@@ -62,11 +78,16 @@ helpers.buildBodyClass = function (req, res, templateData = {}) {
 			}
 		});
 	}
+}
 
+
+function addTemplateBodyClasses(parts, templateData) {
 	if (templateData && templateData.bodyClasses) {
 		parts.push(...templateData.bodyClasses);
 	}
+}
 
+function addStatusAuth(parts, req, res) {
 	parts.push(`page-status-${res.statusCode}`);
 
 	parts.push(`theme-${(meta.config['theme:id'] || '').split('-')[2]}`);
@@ -76,5 +97,19 @@ helpers.buildBodyClass = function (req, res, templateData = {}) {
 	} else {
 		parts.push('user-guest');
 	}
+}
+
+helpers.buildBodyClass = function (req, res, templateData = {}) {
+	const clean = normalizePath(req.path);
+	const parts = buildSegments(clean);
+	const { template } = templateData;
+
+	addTemplateName(parts, template);
+	addTemplateTopic(parts, template, templateData);
+	addTemplateChats(parts, template, templateData);
+	addTemplateBreadcrumbs(parts, templateData);
+	addTemplateBodyClasses(parts, templateData);
+	addStatusAuth(parts, req, res);
+
 	return parts.join(' ');
 };
